93e26f38453d310b54701e6afa7543dd625fdff1
Acshi Haggenmiller <acshikh@gmail.com>
2017-05-31 17:03:35 -0400

    modified motor tester to be for this controller

--- motorTester/motorTester.ino
+++ motorTester/motorTester.ino
@@ -5,28 +5,14 @@
 #define GEAR_BOX_ADDRESS_1 23
 #define GEAR_BOX_ADDRESS_2 22
 
-#define READ_ANGLE_MSG 1
-#define SET_ANGLE_MSG 2
-#define READ_SET_ANGLE_MSG 3
-#define READ_ENCODER_MSG 4
-#define READ_HOMING_MSG 5
-#define DIAGNOSTIC_MSG 6
-#define ADDRESS_MSG 7
-#define VALUE_TEST_MSG 8
-#define READ_ADC0_MSG 9
-#define READ_ADC1_MSG 10
-#define READ_ADC2_MSG 11
-#define READ_ADC3_MSG 12
-#define READ_ADC4_MSG 13
-#define READ_ADC5_MSG 14
-#define READ_ADC6_MSG 15
-#define READ_ADC7_MSG 16
-#define SET_RAW_MOTOR_MSG 17
-#define READ_RAW_MOTOR_MSG 18
-#define SET_TARGET_SPEED_MSG 19
-#define READ_TARGET_SPEED_MSG 20
-#define READ_MEASURED_SPEED_MSG 21
-#define DO_HOMING_MSG 22
+#define SET_RAW_MOTOR_MSG 1
+#define READ_RAW_MOTOR_MSG 2
+#define SET_CURRENT_LIMIT_MSG 3
+#define READ_CURRENT_LIMIT_MSG 4
+#define READ_CURRENT_MSG 5
+#define DIAGNOSTIC_MSG 11
+#define ADDRESS_MSG 12
+#define VALUE_TEST_MSG 13
 
 void setup() {
   // put your setup code here, to run once:
@@ -53,76 +39,60 @@ void sendMessageValue(uint8_t command, int16_t value, int address) {
   Wire.endTransmission();
 }
 
-void actOnByte(int b, int address) {
+void performAction(int16_t action, int address) {
     int16_t value;
-    switch (b) {
-      case 'h':
-        value = sendMessage(READ_HOMING_MSG, address);
-        Serial << "Homing value: " << value << endl;
-        break;
-      case 'e':
-        value = sendMessage(READ_ENCODER_MSG, address);
-        //Serial << "Encoder value: ";
-        Serial << value << endl;
-        break;
-      case 's':
-        value = (int16_t)Serial.parseInt();
-        Serial << "Setting angle to: " << value / 100.0f << endl;
-        sendMessageValue(SET_ANGLE_MSG, value, address);
-        break;
-      case 'a':
-        value = sendMessage(READ_ANGLE_MSG, address);
-        if (value == -1) {
-          Serial << "Cannot read angle: homing not finished" << endl;
-        } else {
-          Serial << "Current angle: " << value / 100.0f << endl;
-        }
-        break;
-      case 'c':
-        value = sendMessage(READ_SET_ANGLE_MSG, address);
-        Serial << "Set angle: " << value << endl;
-        break;
-      case 'd':
-        value = sendMessage(DIAGNOSTIC_MSG, address);
-        Serial << "Diagnostic: " << (char)((value >> 8) & 0xff) << (char)(value & 0xff) << endl;
-        break;
-      case 'w':
-        value = sendMessage(ADDRESS_MSG, address);
-        Serial << "TWI Address: " << value << endl;
-        break;
-      case 't':
-        value = sendMessage(VALUE_TEST_MSG, address);
-        Serial << "Test Value: " << value << endl;
-        break;
-      case 'r':
+    switch (action) {
+      case 'sr':
         value = (int16_t)Serial.parseInt();
         Serial << "Setting raw motor output to: " << value << endl;
         sendMessageValue(SET_RAW_MOTOR_MSG, value, address);
         break;
-      case 'm':
+      case 'gr':
         value = sendMessage(READ_RAW_MOTOR_MSG, address);
-        Serial << "Raw motor output value: " << value << endl;
+        Serial << "Raw motor output: " << value << endl;
         break;
-      case 'p':
+      case 'sl':
         value = (int16_t)Serial.parseInt();
-        Serial << "Setting target speed to (100 = 1 degree per second): " << value << endl;
-        sendMessageValue(SET_TARGET_SPEED_MSG, value, address);
+        Serial << "Setting current limit to: " << value << endl;
+        sendMessageValue(SET_CURRENT_LIMIT_MSG, value, address);
         break;
-    }
-    if (b >= '0' && b <= '7') {
-        value = sendMessage(b - '0' + READ_ADC0_MSG, address);
-        Serial << "ADC " << (char)b << " Value: " << value << endl;
+      case 'gl':
+        value = sendMessage(READ_CURRENT_LIMIT_MSG, address);
+        Serial << "Current limit: " << value << endl;
+        break;
+      case 'cr':
+        value = sendMessage(READ_CURRENT_MSG, address);
+        Serial << "Current: " << value << endl;
+        break;
+      case 'dg':
+        value = sendMessage(DIAGNOSTIC_MSG, address);
+        Serial << "Diagnostic: " << (char)((value >> 8) & 0xff) << (char)(value & 0xff) << endl;
+        break;
+      case 'tw':
+        value = sendMessage(ADDRESS_MSG, address);
+        Serial << "TWI Address: " << value << endl;
+        break;
+      case 'ts':
+        value = sendMessage(VALUE_TEST_MSG, address);
+        Serial << "Test Value: " << value << endl;
+        break;
+      default:
+        Serial << "Command not recognized: " << (char)(action >> 8) << (char)action << endl;
+        break;
     }
 }
 
 void loop() {
- if (Serial.available()) {
+  byte byte0 = 0;
+  byte byte1 = 0;
+  if (Serial.available()) {
     byte b = Serial.read();
+
     if (b == '\n' || b == '\r') {
-      return;
+      performAction((byte0 << 8) & byte1, GEAR_BOX_ADDRESS_1);
+    } else {
+      byte0 = byte1;
+      byte1 = b;
     }
-    actOnByte(b, GEAR_BOX_ADDRESS_1);
-    //actOnByte(b, GEAR_BOX_ADDRESS_2);
   }
-  //actOnByte('e', GEAR_BOX_ADDRESS_1);
 }
